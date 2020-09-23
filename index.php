aeada67781b7c769e820c361c269f7dd9a4781d9
ShetagarPoornima <71587673+ShetagarPoornima@users.noreply.github.com>
2020-09-23 23:13:09 +0530

    planning and testing the input

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+            <form action="index2.php" method="POST" name="sp_login">
+
+    <center>
+        <label>Enter the Dividend</label>
+        <input type="number" name="dividend" placeholder="">
+         <input type="Submit" name="btnsubmit"  value="Claculate">
+    </center>
+            </form>
+    </body>
+</html>
---
+++ index2.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$dividend = $_POST["dividend"];
+echo $dividend;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
